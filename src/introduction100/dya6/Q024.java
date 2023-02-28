717ce7600c016ba59f0f61c96e460ef749261720
ANNA <109578911+joanna930224@users.noreply.github.com>
2023-02-28 23:58:04 +0900

    feat: Programmers day6 - Q024 : 문자 반복 출력하기

---
+++ src/introduction100/dya6/Q024.java
@@ -0,0 +1,24 @@
+package introduction100.dya6;
+
+// 문자 반복 출력하기
+
+// 문제 설명
+// 문자열 my_string과 정수 n이 매개변수로 주어질 때,
+// my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 2 ≤ my_string 길이 ≤ 5
+// 2 ≤ n ≤ 10
+// "my_string"은 영어 대소문자로 이루어져 있습니다.
+
+public class Q024 {
+    public String solution(String my_string, int n) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            for (int j = 0; j < n; j++) {
+                answer.append(my_string.charAt(i));
+            }
+        }
+        return answer.toString();
+    }
+}
